bf5040e4fe0a54787d7d13c9d721f753e1a0abf3
MaudG <maudg35@outlook.fr>
2019-11-06 14:12:08 +0100

    Improve documentation in TestParserHTML

--- Pdl/src/test/testsProjet/TestParserHTML.java
+++ Pdl/src/test/testsProjet/TestParserHTML.java
@@ -1,4 +1,45 @@
 package testsProjet;
 
+import org.junit.Test;
+
+/**
+ * Test the html parsing of a html url
+ */
+
 public class TestParserHTML {
+
+    /**
+     * Creation of test url and test titles
+    */
+   /* String urlValid = "https://en.wikipedia.org/wiki/Comparison_of_Afrikaans_and_Dutch";
+
+    @Test
+    public void testParseHtml(){
+        assertEquals("parse", parseHtml(urlValid));
+    }*/
+
+    /**
+     * Tests parser html :
+     * Pour chacun des cas on teste :
+     *      - bon nb de ligne(s)
+     *      - bon nb de colonne(s)
+     *      - bon contenu
+     *
+     * - tableau sans titre
+     * - tableau avec titre
+     * - tableau html ave  ligne(s) regroupee()s (Comment est compte le bon nombre si les lignes sont regroupees ?)
+     * - tableau html avec colonnes(s) regroupee(s) (Comment est compte le bon nombre si les colonnes sont regroupees ?)
+     * - tableaux vides ?
+     * - tableaux avec une seule case ? (titre ou non titre)
+     * - case avec seulement une image ? ou des images ? ou image(s) et texte ?
+     * - case avec une liste ?
+     * - case avec des icons ?
+     * - case avec des liens ?
+     * - case avec texte contenant des accents ?
+     * - case avec le texte "Les données manquantes sont à compléter." ?
+     * - case avec un texte avec un nombre pour acceder aux notes (lien vers la note) ?
+     * - tableau avec des cases vides et d'autres non vides
+     * - tableau a double entree
+     * - si ya des tris croissants ou dec
+     */
 }
