ce54342d8be808932cb26ecfa0d5b184835dcbbc
Emile <emile.georget@outlook.fr>
2019-11-07 11:50:18 +0100

    add main ability to escape commas and double quotes

--- Pdl/src/main/model/Main.java
+++ Pdl/src/main/model/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         ParserHTML p = new ParserHTML();
         p.setUrlHtml("https://en.wikipedia.org/wiki/Comparison_between_Esperanto_and_Interlingua");
+        p.parseHtml();
 
     }
 
