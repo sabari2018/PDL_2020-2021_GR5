76e5420f73105a52c3928177f42e00ee82359cec
Emile <emile.georget@outlook.fr>
2019-11-07 11:28:51 +0100

    improve main getters and setters, delete constructor add documentation javadoc for getters and setters

--- Pdl/src/main/model/Main.java
+++ Pdl/src/main/model/Main.java
@@ -6,7 +6,9 @@ import model.ParserHTML;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        ParserHTML p = new ParserHTML("https://en.wikipedia.org/wiki/Comparison_between_Esperanto_and_Interlingua");
+        ParserHTML p = new ParserHTML();
+        p.setUrlHtml("https://en.wikipedia.org/wiki/Comparison_between_Esperanto_and_Interlingua");
+
     }
 
 }
