e1fd3a3f3c74402701bf740dcd7fda49f7bd3be0
Yaelle <yaelle.dubois04@orange.fr>
2019-11-23 13:24:55 +0100

    improve test classes : TestConverter.java

--- Pdl/src/test/testsProjet/TestConverter.java
+++ Pdl/src/test/testsProjet/TestConverter.java
@@ -12,164 +12,265 @@ import java.util.*;
 
 public class TestConverter {
 
-        private static HashMap<Table, File> results;
-        private static Converter converter;
+    private static HashMap<Table, File> results;
+    private static Converter converter;
 
-        @BeforeClass
-        public static void setUp () {
-           //Récupérer toutes les Tables extraites et leur associé leur CSV
-            converter = new Converter();
-            ProcessWikiUrl processWikiUrl = new ProcessWikiUrl();
-            processWikiUrl.addWikiUrlFromFile("test", false, "en");
-            processWikiUrl.parseHTML();
-           // processWikiUrl.parseWikiText();
-            List<Table> tableWiki = processWikiUrl.getListTable();
+    /**
+     * Recover all extracted Tables and associates their CSV files
+     */
+    @BeforeClass
+    public static void setUp (){
+        converter = new Converter();
+        ProcessWikiUrl processWikiUrl = new ProcessWikiUrl();
+        processWikiUrl.addWikiUrlFromFile("wikiurls", false, "en");
+        processWikiUrl.parseHTML();
+       // processWikiUrl.parseWikiText();
+        List<Table> tableWiki = processWikiUrl.getListTable();
 
-            results = new HashMap<Table, File>();
-            File file;
-            for (Table table : tableWiki) {
-                converter.convertToCSV(table);
-                file = converter.getCsvConvertFile();
-                results.put(table,file);
+        results = new HashMap<Table, File>();
+        File file;
+        for (Table table : tableWiki) {
+            converter.convertToCSV(table);
+            file = converter.getCsvConvertFile();
+            results.put(table,file);
+        }
+    }
+
+    /**
+     * Test if the CSV file is created and filled
+     */
+    @Test
+    public void testFileIsCreated () {
+        int nbFileFilled = 0;
+        ArrayList<String> messageErrors = new ArrayList<String>();
+        String message;
+
+        for (Map.Entry<Table, File> entry : results.entrySet()){
+            Table table = entry.getKey();
+            File file = entry.getValue();
+
+            if (converter.fileIsFilled(file)) {
+                nbFileFilled ++;
+            }
+            else {
+                message = "The CSV file for the  table n "+ table.getNumTable()+" of the page "
+                            + table.getTitle() + " for the "+ table.getExtractionType()+" extraction type is not filled";
+                messageErrors.add(message);
             }
         }
+        displayFileErrors(messageErrors);
+        Assert.assertEquals ("All files are not filled : "+nbFileFilled+ " have been filled on "+results.size(),results.size(), nbFileFilled);
+    }
 
-        @Test
-        public void testFileIsCreated () {
-            int nbFileFilled = 0;
-            ArrayList<String> messageErrors = new ArrayList<String>();
-            String message;
+    /**
+     * Check if there the same row number in the table and the CSV
+     */
+    @Test
+    public void checkNbRows () {
+        int nbFileCorrect = 0;
+        ArrayList<String> messageErrors = new ArrayList<String>();
+        String message;
 
-            for (Map.Entry<Table, File> entry : results.entrySet()){
-                Table table = entry.getKey();
-                File file = entry.getValue();
+        for (Map.Entry<Table, File> entry : results.entrySet()){
+            Table table = entry.getKey();
+            File file = entry.getValue();
 
-                if (converter.fileIsFilled(file)) {
-                    nbFileFilled ++;
-                }
-                else {
-                    message = "Le fichier csv pour la page "+ table.getTitle()+"n'a pas été rempli et le tableau n°"
-                            + table.getNumTable() + "pour l'extraction de type "+ table.getExtractionType();
-                    messageErrors.add(message);
-                }
+            if (getNbRowsInTheTable(table) == getNbRowsInTheCSV(file)) {
+                nbFileCorrect ++;
             }
-            for (String error : messageErrors) {
-                System.out.println(error);
+            else {
+                message = "The number of rows in the table n° "+table.getNumTable()+" of the page "+table.getTitle()+
+                            " for the "+table.getExtractionType()+ " extraction type does not match";
+                messageErrors.add(message);
             }
-            Assert.assertEquals ("Tous les fichiers n'ont pas été remplis, "+nbFileFilled+ " ont été remplis sur "+results.size(),results.size(), nbFileFilled);
-
         }
+        displayFileErrors(messageErrors);
+        Assert.assertEquals("All files do not contain the correct number of rows : "+nbFileCorrect+
+                                                " files contain the right number of rows on "+results.size(),results.size(), nbFileCorrect);
+    }
 
-       @Test
-        public void checkNbRows () {
-           int nbFileCorrect = 0;
-           ArrayList<String> messageErrors = new ArrayList<String>();
-           String message;
+    /**
+     * Check if there the same column number in the table and the CSV
+     */
+    @Test
+    public void checkNbColumn () {
+        int nbFileCorrect = 0;
+        ArrayList<String> messageErrors = new ArrayList<String>();
+        String message;
 
-            for (Map.Entry<Table, File> entry : results.entrySet()){
-                Table table = entry.getKey();
-                File file = entry.getValue();
+        for (Map.Entry<Table, File> entry : results.entrySet()) {
+            Table table = entry.getKey();
+            File file = entry.getValue();
 
-                if (getNbRowsInTheTable(table) == getNbRowsInTheCSV(file)) {
-                    nbFileCorrect ++;
-                }
-                else {
-                    message = "Le nombre de ligne du tableau n°"+table.getNumTable()+" de la page "+table.getTitle()+
-                            " pour l'extraction de type "+table.getExtractionType()+ " ne correspond pas";
-                    messageErrors.add(message);
-                }
+            ArrayList<Integer> nbColumnTable = getNbColumnInTheTable(table);
+            ArrayList<Integer> nbColumnFile = getNbColumnsInTheCSV(file);
+
+            if (nbColumnTable.containsAll(nbColumnFile)) {
+                nbFileCorrect++;
+            } else {
+                message = "The number of columns in the table n°" + table.getNumTable() + " of the page " + table.getTitle() +
+                        " for the " + table.getExtractionType() + " extraction type does not match";
+                messageErrors.add(message);
             }
-           for (String error : messageErrors) {
-               System.out.println(error);
-           }
-            Assert.assertEquals("Tout les fichiers ne contienent pas le bon nombre de lignes : "+nbFileCorrect+
-                                                " contiennent le bon nombre de lignes sur "+results.size(),results.size(), nbFileCorrect);
         }
+        displayFileErrors(messageErrors);
+        Assert.assertEquals("All files do not contain the correct number of columns :" + nbFileCorrect +
+                " files contain the right number of columns on " + results.size(), results.size(), nbFileCorrect);
+    }
 
-        @Test
-        public void checkNbColumn () {
-            int nbFileCorrect = 0;
-            ArrayList<String> messageErrors = new ArrayList<String>();
-            String message;
+    /**
+     * Check if the CSV is valid -> same number of column in each row
+     */
+    @Test
+    public void testCsvValid () {
+        int nbFileCorrect = 0;
+        String message;
+        ArrayList<String> errorsMessage = new ArrayList<String>();
 
-            for (Map.Entry<Table, File> entry : results.entrySet()){
-                Table table = entry.getKey();
-                File file = entry.getValue();
-
-                if (getNbColumnInTheTable(table).containsAll(getNbColumnsInTheCSV(file))) {
-                    nbFileCorrect ++;
-                }
-                else {
-                    message = "Le nombre de colonne du tableau n°"+table.getNumTable()+" de la page "+table.getTitle()+
-                            " pour l'extraction de type "+table.getExtractionType()+ " ne correspond pas";
-                    messageErrors.add(message);
-                }
-            }
-            for (String error : messageErrors) {
-                System.out.println(error);
-            }
-            Assert.assertEquals("Tout les fichiers ne contienent pas le bon nombre de colonnes: "+nbFileCorrect+
-                    " contiennent le bon nombre de colonnes sur "+results.size(),results.size(), nbFileCorrect);
-        }
-
-        private ArrayList getNbColumnInTheTable (Table table) {
-            ArrayList<Integer> nbColumn = new ArrayList<Integer>();
-            HashMap<Integer, String []> content = table.getContent();
-
-            for (Map.Entry entry : content.entrySet()) {
-                String [] row = (String[]) entry.getValue();
-                nbColumn.add(row.length);
-            }
-            return nbColumn;
-        }
-
-        private int getNbRowsInTheTable (Table table) {
-            return table.getContent().size();
-        }
-
-        private static int getNbRowsInTheCSV (File file) {
-            int nbRows = 0;
-            try {
-                FileReader f = new FileReader(file);
-                BufferedReader buffered = new BufferedReader(f);
-                String line = buffered.readLine();
-                while (line != null) {
-                    nbRows ++;
-                    line = buffered.readLine();
-                }
-                buffered.close();
-            }
-            catch (IOException e) {
-                System.out.println (e);
-            }
-            return nbRows;
-        }
-
-        private ArrayList getNbColumnsInTheCSV (File file) {
-            ArrayList<Integer> nbColumns = new ArrayList<Integer>();
+        for (Map.Entry<Table, File> entry : results.entrySet()) {
+            Table table = entry.getKey();
+            File file = entry.getValue();
+            int nbCorrectLine = 0;
 
             try {
                 FileReader f = new FileReader(file);
                 BufferedReader buffered = new BufferedReader(f);
                 String line = buffered.readLine();
+                int nbColumnRef = getNbCommas(line);
+                line = buffered.readLine();
+
                 while (line != null) {
-                    nbColumns.add(line.split(",").length);
+                    int nbColumn = getNbCommas(line);
+                    if (nbColumn == nbColumnRef) {
+                        nbCorrectLine++;
+                    }
                     line = buffered.readLine();
                 }
                 buffered.close();
+
+                if (nbCorrectLine == getNbRowsInTheCSV(file)-1) {
+                    nbFileCorrect++;
+                }
+                else {
+                    message = "The CSV file is not valid for the table n°"+table.getNumTable()+" of the page "+table.getTitle()+
+                            " for the "+table.getExtractionType()+ " extraction type";
+                    errorsMessage.add(message);
+                }
             }
             catch (IOException e) {
                 System.out.println (e);
             }
-
-            return nbColumns;
         }
-/*
-        @AfterClass
-        public static void deleteFile () {
-            for (Map.Entry<Table, File> entry : results.entrySet()){
-                File file = entry.getValue();
-                file.delete();
+        displayFileErrors(errorsMessage);
+        Assert.assertEquals("There are: "+nbFileCorrect+ " CSV valid on "+results.size(),results.size(), nbFileCorrect);
+    }
+
+    /**
+     * Display information about table which contains erros
+     * @param errorsMessage an arrayList which contains all errors messages
+     */
+    private void displayFileErrors (ArrayList<String> errorsMessage) {
+        for (String error : errorsMessage) {
+            System.out.println(error);
+        }
+    }
+
+    /**
+     * Find the number of commas in the line. Commas in the quotation
+     * marks are not recorded
+     * @param line
+     * @return the number of commas in a line
+     */
+    private int getNbCommas (String line) {
+        String test = line.replaceAll("(\"[^\"]*\")","");
+        int nbCommas = 0;
+
+        for (int i = 0; i < test.length(); i++) {
+            if (test.charAt(i) == ',') {
+                nbCommas++;
             }
-        }*/
+        }
+
+        return nbCommas;
+    }
+
+    /**
+     *
+     * @param table
+     * @return an arrayList which contains the number of columns for each row in the Table
+     */
+    private ArrayList getNbColumnInTheTable (Table table) {
+        ArrayList<Integer> nbColumn = new ArrayList<Integer>();
+        HashMap<Integer, String []> content = table.getContent();
+
+        for (Map.Entry entry : content.entrySet()) {
+            String [] row = (String[]) entry.getValue();
+            nbColumn.add(row.length);
+        }
+        return nbColumn;
+    }
+
+    /**
+     *
+     * @param table
+     * @return the number of rows in a Table
+     */
+    private int getNbRowsInTheTable (Table table) {
+        return table.getContent().size();
+    }
+
+    /**
+     *
+     * @param file
+     * @return the number of row in a file
+     */
+    private static int getNbRowsInTheCSV (File file) {
+        int nbRows = 0;
+        try {
+            FileReader f = new FileReader(file);
+            BufferedReader buffered = new BufferedReader(f);
+            String line = buffered.readLine();
+            while (line != null) {
+                nbRows ++;
+                line = buffered.readLine();
+            }
+            buffered.close();
+        }
+        catch (IOException e) {
+            System.out.println (e);
+        }
+        return nbRows;
+    }
+
+    /**
+     *
+     * @param file
+     * @return an ArrayList which contains the number of columns for each rows
+     */
+    private ArrayList getNbColumnsInTheCSV (File file) {
+        ArrayList<Integer> nbColumns = new ArrayList<Integer>();
+
+        try {
+            FileReader f = new FileReader(file);
+            BufferedReader buffered = new BufferedReader(f);
+            String line = buffered.readLine();
+            while (line != null) {
+                nbColumns.add(getNbCommas(line)+1);
+                line = buffered.readLine();
+            }
+            buffered.close();
+        }
+        catch (IOException e) {
+            System.out.println (e);
+        }
+        return nbColumns;
+    }
+
+   /* @AfterClass
+    public static void deleteFile () {
+        for (Map.Entry<Table, File> entry : results.entrySet()){
+            File file = entry.getValue();
+            file.delete();
+        }
+    }*/
 }
